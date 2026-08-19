533cdbe1c1c9a4ccc90c1085b21149334e74c960
MJ <vovodla74@gmail.com>
2026-08-19 16:44:47 +0900

    Initial commit

---
+++ src/main/java/io/github/devup/tripfinder/TripFinderApplication.java
@@ -0,0 +1,13 @@
+package io.github.devup.tripfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripFinderApplication.class, args);
+    }
+
+}
